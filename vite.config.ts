0514d6d9e1c0edc0e5fc2ea36ea2e8032f27f314
andreirhamni09 <andreirhamni09@gmail.com>
2025-06-23 13:24:56 +0700

    test github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: 'https://github.com/andreirhamni09/vue-github-pages.git', // wajib diisi agar path benar
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
